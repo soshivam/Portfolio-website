7c9698e2d71af68434b863cbe6668fad1e88f0de
soshivam <151746955+soshivam@users.noreply.github.com>
2024-10-02 00:25:28 +0530

    Add files via upload
    My portfolio website is an interactive platform showcasing my skills
    and projects. Using HTML, CSS, and JavaScript, it features sections
     for home, about, services, projects, and contact. The design is
     responsive, ensuring a seamless experience on all devices, and
     includes a mobile menu for easy navigation, highlighting my web
     development capabilities.

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('menu-icon').addEventListener('click', function () {
+    const navList = document.querySelector('.navbar');
+  
+    navList.classList.toggle('active');
+});
